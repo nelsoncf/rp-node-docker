76245330d7d70c726793f56be1273c504c7a87d8
Nelson Campos <ncamposfh@gmail.com>
2019-06-11 22:48:12 -0300

    docker + nodemon

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const HOST = "0.0.0.0";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Raiooo Silver!!!!");
+  res.send("Raiooo Silver!!!!. Vraaaauu");
 });
 
 app.listen(PORT, HOST);
